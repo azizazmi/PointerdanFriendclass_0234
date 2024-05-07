9b82bd29a3af8850098e1758f0b3706a6410a4de
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-05-07 14:49:05 +0700

    menambahkan class persegiPanjang

--- pointer.cpp
+++ pointer.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+class persegiPanjang {
+    public:
+    int panjang, lebar;
+    int luasPersegiPanjang(int b, int a) {
+        return a * b;
+    }
+};
